ac309463e95642f3410444e4a432e953fbfcba74
Param Yonzon <param.yonzon14@stjohns.edu>
2022-12-25 02:24:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("""Param
+December 22, 2022
+
+I am signing up for 100 days of code in python, so one day I can become an official software engineer.""")
+
+print ("I am feeling detemined!")
+
+print("You can follow my progress at https://replit.com/@ParamYonzon")
